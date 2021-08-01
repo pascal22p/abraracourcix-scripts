1c78ec3a6bacd2ed541fd58fd6d41c73b8df12bc
Pascal Parois <pascal22p@parois.net>
2021-08-01 13:18:08 +0000

    sink done

--- graphiteSink.php
+++ graphiteSink.php
@@ -1,9 +1,26 @@
 <?php
-// Get the JSON contents
-$json = file_get_contents('php://input');
+error_reporting(E_ALL);
 
-// decode the json data
-$data = json_decode($json);
+$data = trim(file_get_contents('php://input')).PHP_EOL;
 
-echo $data;
+try {
+    $fp = fsockopen("tcp://127.0.0.1", 2003, $errno, $errstr);
+
+    if (!empty($errno)) echo $errno;
+    if (!empty($errstr)) echo $errstr;
+
+    $bytes = fwrite($fp, $data);
+    if($bytes == strlen($data)) {
+      echo $data;
+      http_response_code(202);
+    } else {
+      echo "Size does not match";
+      http_response_code(500);
+    }
+} catch (Exception $e) {
+    echo "\nNetwork error: ".$e->getMessage();
+    http_response_code(500);
+}
+
+
 ?>
